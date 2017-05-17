d5f38be159218ae6751485c6083c2a400f8a5dfc
yuwei <yuwei@toastdogs.com>
2017-05-17 13:58:55 -0700

    Add sample csv

---
+++ myio.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import random
+
+def sample_read(out, n, s, seed=42):
+	random.seed(seed)
+    skip = sorted(random.sample(xrange(1, n + 1), n - s))
+    return pd.read_csv(out, skiprows=skip)
