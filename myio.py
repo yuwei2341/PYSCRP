60ffa7dc215b6015868c8e58ae2bcd49036e7d5d
yuwei <yuwei@toastdogs.com>
2017-05-17 17:16:42 -0700

    indent

--- myio.py
+++ myio.py
@@ -4,5 +4,5 @@ import random
 
 def sample_read(out, n, s, seed=42):
 	random.seed(seed)
-    skip = sorted(random.sample(xrange(1, n + 1), n - s))
-    return pd.read_csv(out, skiprows=skip)
+	skip = sorted(random.sample(xrange(1, n + 1), n - s))
+	return pd.read_csv(out, skiprows=skip)
