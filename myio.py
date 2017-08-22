57c28a9a9fd8a7e7b2636224b3eacbaac4707875
yuwei2341@hotmail.com <yuwei@uber.com>
2017-08-22 16:39:31 -0700

    Add queryrunner parameters

--- myio.py
+++ myio.py
@@ -8,3 +8,9 @@ def sample_read(out, n, s, seed=42):
 	random.seed(seed)
 	skip = sorted(random.sample(xrange(1, n + 1), n - s))
 	return pd.read_csv(out, skiprows=skip)
+
+# parameters for query_runner
+import socket
+qr_params = {'user_email': 'yuwei@uber.com'}
+if socket.gethostname() == 'yuwei-C02SH0TQG8WL':
+    qr_params['hdfs_proxy_port'] = 9999
